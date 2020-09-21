565dcb2667d00982e24299c1a87ead9ca673f7bc
ultraultimated <neelp1543@gmail.com>
2020-09-20 21:46:29 -0400

    added config.py file

---
+++ code/config.py
@@ -0,0 +1,10 @@
+elasticsearch_host = "localhost"
+elasticsearch_port = 9200
+elasticsearch_user = ""
+elasticsearch_password = ""
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+x_rapidapi_key = ""
