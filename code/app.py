13703ed78afb39e2f7a2d7628d3a8a509e04ca92
binpatel31 <binpatel31@gmail.com>
2020-09-21 23:19:02 -0400

    added code to automate data sending - app.py

--- code/app.py
+++ code/app.py
@@ -2,10 +2,12 @@ import pandas as pd
 import json
 import threading
 import uuid
-
+import elk
 from pathlib import Path
 from kafka import KafkaProducer, KafkaConsumer
 from time import sleep
+from pytz import timezone
+import datetime
 
 PATH = Path('../Data/')
 KAFKA_HOST = 'localhost:9092'
@@ -18,6 +20,33 @@ df_real['json'] = df_real.apply(lambda x: x.to_json(), axis = 1)
 # load the json objects
 messages = df_real.json.tolist()
 
+def current_time():
+    eastern = timezone('US/Eastern')
+    fmt = '%Y-%m-%d %H:%M:%S'
+    return datetime.datetime.now().astimezone(eastern).strftime(fmt)
+
+def send_to_es(filename):
+    f = open(filename)
+    elk.create_index_predictions(f, "predictions")
+
+def add_to_json(price):
+    '''
+    function to add last 120 minutes of predicted data to json file
+    INPUT: stock price
+    OUTPUT: json file prediction.json
+    '''
+    try:
+        f = open("prediction.json")
+        data = json.load(f)
+        if len(data)>=120:
+            del data[0]
+    except:
+        data = []
+    dic = {"created_at": current_time(), "prediction": price}
+    data.append(dic)
+    with open('prediction.json', 'w') as fout:
+        json.dump(data, fout)
+    send_to_es("prediction.json")
 
 def start_producing():
     """Kafka producer
@@ -46,6 +75,8 @@ def start_consuming():
         if 'prediction' in message:
             request_id = message['request_id']
             print("--- CONSUMER: Received prediction {} for request id {}".format(message['prediction'], request_id))
+            add_to_json(message['prediction'])
+
 
 threads = []
 pthread = threading.Thread(target=start_producing)
