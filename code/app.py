f37fa5956c275be01fba347d8faf8b038ddc99d8
binpatel31 <binpatel31@gmail.com>
2020-09-21 23:29:30 -0400

    updated app.py to automate data transfer

--- code/app.py
+++ code/app.py
@@ -2,22 +2,51 @@ import pandas as pd
 import json
 import threading
 import uuid
-
+import elk
 from pathlib import Path
 from kafka import KafkaProducer, KafkaConsumer
 from time import sleep
+from pytz import timezone
+import datetime
 
-PATH = Path('../data/Simulation_Data')
+PATH = Path('../Data/')
 KAFKA_HOST = 'localhost:9092'
 
 # load the data that is captured real time
-df_real = pd.read_csv(PATH/'NKE.csv')
+df_real = pd.read_csv(PATH/'NKE_test.csv')
 
 df_real['json'] = df_real.apply(lambda x: x.to_json(), axis = 1)
 
 # load the json objects
 messages = df_real.json.tolist()
 
+def current_time():
+    eastern = timezone('US/Eastern')
+    fmt = '%Y-%m-%d %H:%M:%S'
+    return datetime.datetime.now().astimezone(eastern).strftime(fmt)
+
+def send_to_es(filename):
+    f = open(filename)
+    elk.create_index_predictions(f, "predictions")
+
+def add_to_json(price):
+    '''
+    function to add last 120 minutes of predicted data to json file
+    INPUT: stock price
+    OUTPUT: json file prediction.json
+    '''
+    try:
+        f = open("prediction.json")
+        data = json.load(f)
+        if len(data)>=120:
+            del data[0]
+    except:
+        data = []
+    dic = {"created_at": current_time(), "prediction": price}
+    data.append(dic)
+    with open('prediction.json', 'w') as fout:
+        json.dump(data, fout)
+    send_to_es("prediction.json")
 
 def start_producing():
     """Kafka producer
@@ -31,7 +60,7 @@ def start_producing():
         producer.send('app_messages', json.dumps(message).encode('utf-8'))
         producer.flush()
         print("+++ PRODUCER: Sent message with id {}".format(message_id))
-        sleep(2)
+        sleep(60)
 
 
 def start_consuming():
@@ -46,6 +75,8 @@ def start_consuming():
         if 'prediction' in message:
             request_id = message['request_id']
             print("--- CONSUMER: Received prediction {} for request id {}".format(message['prediction'], request_id))
+            add_to_json(message['prediction'])
+
 
 threads = []
 pthread = threading.Thread(target=start_producing)
